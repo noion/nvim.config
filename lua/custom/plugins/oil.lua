3869e313e9aa5e5a83b638181e5f2d316a178074
noion <n3t1m3n@gmail.com>
2025-07-18 23:36:52 +0300

    oil floation window

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -22,6 +22,34 @@ return {
       ['g.'] = { 'actions.toggle_hidden', mode = 'n' },
       ['g\\'] = { 'actions.toggle_trash', mode = 'n' },
     },
+    vim.keymap.set('n', '<leader>o', function()
+      require('oil').open_float()
+    end, { desc = 'Open oil in a floating window' }),
+    -- Configuration for the floating window in oil.open_float
+    float = {
+      -- Padding around the floating window
+      padding = 2,
+      -- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+      max_width = 0,
+      max_height = 0,
+      border = 'rounded',
+      win_options = {
+        winblend = 0,
+      },
+      -- optionally override the oil buffers window title with custom function: fun(winid: integer): string
+      get_win_title = nil,
+      -- preview_split: Split direction: "auto", "left", "right", "above", "below".
+      preview_split = 'auto',
+      -- This is the config that will be passed to nvim_open_win.
+      -- Change values here to customize the layout
+      override = function(conf)
+        return conf
+      end,
+    },
+    view_options = {
+      -- Show files and directories that start with "."
+      show_hidden = true,
+    },
   },
   -- Optional dependencies
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
